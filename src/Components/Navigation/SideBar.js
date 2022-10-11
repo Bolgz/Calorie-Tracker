1ea489871763282ae02a6bdb5bcf0ac2900eb38c
Marco Freemantle <marcofreemantle@gmail.com>
2022-10-11 18:03:01 +0100

    Changed navigation bar

--- src/Components/Navigation/SideBar.js
+++ src/Components/Navigation/SideBar.js
@@ -1,9 +1,16 @@
 import "./SideBar.css";
 import { getAuth, signOut } from "firebase/auth";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { FaGem } from "react-icons/fa";
+import {
+  FaStar,
+  FaCog,
+  FaChartPie,
+  FaChartBar,
+  FaDoorOpen,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import logo from "../../flamelogo.png";
 
 function SideBar() {
   /**
@@ -22,31 +29,37 @@ function SideBar() {
 
   return (
     <ProSidebar>
-      <Menu iconShape="square">
+      <Link to="/" className="title-link">
+        <img src={logo} alt="logo" width="158" height="85" className="logo" />
         <h2 className="navigation-title">Calorie Tracker</h2>
-        <MenuItem icon={<FaGem />}>
+      </Link>
+      <Menu iconShape="circle">
+        <MenuItem icon={<FaChartPie />} className="menuItem">
           <Link to="/" className="link">
             Dashboard
           </Link>
         </MenuItem>
-        <MenuItem icon={<FaGem />}>
+        <MenuItem icon={<FaChartBar />} className="menuItem">
           <Link to="/progress" className="link">
             Progress
           </Link>
         </MenuItem>
-        <MenuItem icon={<FaGem />}>
+        <MenuItem icon={<FaStar />} className="menuItem">
           <Link to="/Goals" className="link">
             Goals
           </Link>
         </MenuItem>
-        <MenuItem icon={<FaGem />}>
+        <MenuItem icon={<FaCog />} className="menuItem">
           <Link to="/account" className="link">
             Account
           </Link>
         </MenuItem>
-        <MenuItem icon={<FaGem />}>
+
+        <MenuItem icon={<FaDoorOpen />} className="menuItem">
           <Button variant="none" className="logout-button" onClick={Logout}>
-            Log out
+            <Link to="/login" className="link">
+              Log out
+            </Link>
           </Button>
         </MenuItem>
       </Menu>
